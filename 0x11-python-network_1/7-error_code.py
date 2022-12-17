c4704439910442ff487457f25fae38398a51b4b0
Stella <stella.me90@gmail.com>
2022-12-17 07:23:32 -0800

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+"""Display response body
 """
-import sys
+
 import requests
+from sys import argv
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
         print(r.text)
+    except:
+        print('Error code: {}'.format(r.status_code))
